efbb42218aea7fad648d42392d4378bc6fcdd590
theoj <theoj@12600KF_BUILD>
2024-09-18 16:21:16 +0100

    Add project files.

---
+++ PrimeNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿//Prime Numbers
+
+using System.Transactions;
+
+int CheckCurrent = 1;
+
+Console.WriteLine("The first few prime numbers are : ");
+
+for (int Current = 0; Current < 50; Current++)
+{
+    CheckCurrent = 2;
+    bool Prime = true;
+    while (CheckCurrent * CheckCurrent <= Current)
+    {
+        if (Current % CheckCurrent == 0)
+        {
+            Prime = false;
+        }
+        CheckCurrent++;
+    }
+    if (Prime == true)
+    {
+        Console.WriteLine(Current);
+    }
+}
+
+//Used boolean instead of string "Yes" and "No" from pseudocode
+//also imo better variable names compared to "Count1" and "Count2"
